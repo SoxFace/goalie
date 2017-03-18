e29b973ed96f9d828cdb6accab0985a5414fbda0
soxface <sonyacorcoran@gmail.com>
2017-03-18 23:26:06 +1100

    styled stats card

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -3,7 +3,22 @@ import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Car
 import RaisedButton from 'material-ui/RaisedButton';
 import latha from '../../public/latha.jpeg';
 import sydneyfc from '../../public/sydneyfc.png';
+import {blue100} from 'material-ui/styles/colors';
 
+// Back page
+import Chip from 'material-ui/Chip';
+import Avatar from 'material-ui/Avatar';
+
+const styles = {
+  chip: {
+    margin: 4
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    margin: 'auto'
+  }
+};
 
 export default class PlayerCard extends Component{
 
@@ -48,14 +63,50 @@ export default class PlayerCard extends Component{
           overlay={<CardTitle title="Latha Back" subtitle="Defender" />}
         >
         </CardMedia>
-        <CardTitle title="Club Stats" subtitle="MVP Stats" />
-        <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-        </CardText>
+        <CardTitle title="Club Stats" />
+          <div style={styles.wrapper}>
+            <Chip style={styles.chip}>
+              <Avatar size={32}>'15</Avatar>
+              Debut Year
+            </Chip>
+            <Chip style={styles.chip}>
+              <Avatar size={32}>27</Avatar>
+              Age
+            </Chip>
+            <Chip style={styles.chip}>
+              <Avatar size={32}>5'4</Avatar>
+              Height
+            </Chip>
+            <Chip style={styles.chip}>
+              <Avatar size={32}>2</Avatar>
+              Previous Clubs
+            </Chip>
+            <Chip style={styles.chip}>
+              <Avatar size={32}>37</Avatar>
+              Games Played
+            </Chip>
+            <Chip style={styles.chip}>
+              <Avatar size={32}>2</Avatar>
+              MVP's Earned
+            </Chip>
+            <Chip style={styles.chip}>
+              <Avatar size={32}>15</Avatar>
+              Goals
+            </Chip>
+          </div>
 
+        <CardTitle title="Currently Working On"/>
+            <div style={styles.wrapper}>
+            <Chip style={styles.chip} backgroundColor={blue100}>
+              1 on 1
+            </Chip>
+            <Chip style={styles.chip} backgroundColor={blue100}>
+              Ball Control
+            </Chip>
+            <Chip style={styles.chip} backgroundColor={blue100}>
+              Speed
+            </Chip>
+          </div>
         <CardActions>
           <RaisedButton primary={true} fullWidth={true}label="FLIP ME" />
         </CardActions>
